86a5ab39c148b8fed9ee89b20e08defa6c43f4c8
Aleksander Okrasa <olekokrasa@gmail.com>
2023-01-31 00:46:23 +0100

    wersja robocza - dodanie funkcji usuwajacej nie potrzebne informacje o odobserwowanych stronach i dodanie funkcji usuwajacej z obserwowanych niechcianych osób

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 
 # login = ''
 # password = ''
+nazwisko = 'Okrasa'
 driver = webdriver.Chrome()
 def start():
     driver.get("https://www.facebook.com")
@@ -25,7 +26,7 @@ def start():
     input("kliknij enter jak przejdziesz do strony glownej")
 
 
-def unfollow():
+def unfollow_webs():
     try:
         print('wczytuje strone...')
         #przejscie do zakładki - Dziennik aktywnosci / Kontakty / Strony, polubienia stron i zainteresowania"
@@ -61,11 +62,85 @@ def unfollow():
     except:
         print("Blad z wczytywaniem strony")
 
+def delete_unfollows_from_view():
+    # przejscie do zakładki - Dziennik aktywnosci / Kontakty / Połączenia
+    driver.get('https://www.facebook.com/100002743730769/allactivity?activity_history=false&category_key=FOLLOWCLUSTER&manage_mode=false&should_load_landing_page=false')
+    three_dots = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Opcje działania"]')
+    for i in range(0, 1000):
+        try:
+            try:
+                three_dots[0].click()
+            except:
+                three_dots = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Opcje działania"]')
+
+                three_dots[0].click()
+            finally:
+                driver.find_element(By.CSS_SELECTOR, '[role="menuitem"]').click()   # Kliknięcie przycisku "Usuń"
+                loaded = expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="Usuń"]'))
+                WebDriverWait(driver, 5).until(loaded)
+                driver.find_element(By.CSS_SELECTOR, '[aria-label="Usuń"]').click() #podtwierdzenie usunięcia
+                print(f"usunięto stronę o indexie {i+1}")
+        except:
+            # przejscie do zakładki - Dziennik aktywnosci / Kontakty / Połączenia
+            driver.get('https://www.facebook.com/100002743730769/allactivity?activity_history=false&category_key=FOLLOWCLUSTER&manage_mode=false&should_load_landing_page=false')
+            loaded = expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '[aria-label="Opcje działania"]'))
+
+            WebDriverWait(driver, 15).until(loaded)
+            time.sleep(1)
+            print("Ponownie załadowano stronę")
+
+
+
+
+        # # czekanie na załadowanie przycisku "Nie lubię"
+        # loaded = expected_conditions.presence_of_element_located((By.CSS_SELECTOR, '[role="menuitem"]'))
+        # WebDriverWait(driver, 5).until(loaded)
+        # # delete = driver.find_element(By.CSS_SELECTOR, '[role="menuitem"]')
+        # # confirm = driver.find_elements(By.CSS_SELECTOR, '[aria-label="Usuń"]')
+        # options[0].click()
+        # time.sleep(0.1)
+        # driver.find_element(By.CSS_SELECTOR, '[role="menuitem"]').click()
+        # time.sleep(0.1)
+        # driver.find_element(By.CSS_SELECTOR, '[aria-label="Usuń"]').click()
+        # time.sleep(0.1)
+def unfollow_persons():
+    # przejscie do zakładki - Dziennik aktywnosci / Kontakty
+    driver.get('https://www.facebook.com/100002743730769/allactivity/?activity_history=false&category_key=YOURCONNECTIONS&manage_mode=false&should_load_landing_page=false')
+
+    #driver.get('https://www.facebook.com/100002743730769/allactivity?activity_history=false&category_key=FOLLOWCLUSTER&manage_mode=false&should_load_landing_page=false')
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")  # przescrollowanie na sam dół strony
+    persons = driver.find_elements(By.PARTIAL_LINK_TEXT, f'{nazwisko} obserwuje użytkownika')
+
+    #printowanie
+    list = []
+    list_of_exceptions = ('Paulina', 'Magdalena', 'Politechnik', 'Wiktoria', 'Kamila', 'Marcelina')
+    for person in persons:
+        if not person.text[40:-16].startswith(list_of_exceptions):
+            list.append(person.text[40:-16])
+        else:
+            print(f'Pominięto osobę {person.text[40:-16]}')
+
+    #stworzenie słownika - nazwa strony(klucz) i link(wartość
+    dictionary = {}
+    list_of_exceptions = ('Paulina', 'Magdalena', 'Politechnik', 'Wiktoria', 'Kamila', 'Marcelina')
+    for person in persons:
+        if not person.text[40:-16].startswith(list_of_exceptions):
+            dictionary[person.text[40:-16]] = person.get_attribute('href')
+        else:
+            print(f'Pominięto osobę {person.text[40:-16]} o linku {person.get_attribute("href")}')
+
+    print(dictionary)
+
 
 start()
+# delete_unfollows_from_view()
+# unfollow_webs()
 for i in range(100):
     print(f'odpalanie funkcji unfollow poraz {i}')
     try:
-        unfollow()
+        # unfollow_webs()
+        pass
     except:
         print('ERROR')
+
+
